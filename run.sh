e983e4767997406036e8937e3e5b571d799c60a8
konradko <konrad@korzel.com>
2018-06-13 15:12:18 +0100

    Use APP_PATH and JAVA_OPTS instead of run command

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ VERSION=1.0.0
 wget https://github.com/alphagov/verify-service-provider/releases/download/$VERSION/verify-service-provider-$VERSION.zip
 unzip verify-service-provider-$VERSION.zip
 rm verify-service-provider-$VERSION.zip
-cd ./verify-service-provider-$VERSION
-./bin/verify-service-provider server verify-service-provider.yml
+export APP_PATH="./verify-service-provider-$VERSION/bin/verify-service-provider"
+export JAVA_OPTS="server config.yml"
